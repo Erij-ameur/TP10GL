f137eba500bc3fba343c66e71ca41777086dbd41
Erij Ameur <93911431+Erij-ameur@users.noreply.github.com>
2023-11-06 14:39:50 +0100

    first commit Erij

--- src/main/java/agenda/Event.java
+++ src/main/java/agenda/Event.java
@@ -33,6 +33,15 @@ public class Event {
         this.myDuration = duration;
     }
 
+    @Override
+    public String toString() {
+        return "Event{" +
+                "myTitle='" + myTitle + '\'' +
+                ", myStart=" + myStart +
+                ", myDuration=" + myDuration +
+                '}';
+    }
+
     /**
      * Tests if an event occurs on a given day
      *
@@ -40,8 +49,7 @@ public class Event {
      * @return true if the event occurs on that day, false otherwise
      */
     public boolean isInDay(LocalDate aDay) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        return !(aDay.isBefore(this.getStart().toLocalDate()) || aDay.isAfter(this.getStart().plus(this.getDuration()).toLocalDate()));
     }
    
     /**
